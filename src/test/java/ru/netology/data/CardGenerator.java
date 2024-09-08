bde1b15c75187f9a59fd7ea22e340d98709d126e
Tatiana Titova <ritasom666@gmail.com>
2024-09-08 19:31:35 +0400

    start tests

--- src/test/java/ru/netology/data/CardGenerator.java
+++ src/test/java/ru/netology/data/CardGenerator.java
@@ -20,7 +20,29 @@ public class CardGenerator {
 
     private static String randomName (){
         Faker faker = new Faker(new Locale("en"));
-        return faker.name().fullName();
+        return faker.name().firstName()+" "+faker.name().lastName();
+
+    }
+
+    private static String invalidName1(){
+        Faker faker = new Faker(new Locale("en"));
+        return faker.name().firstName();
+
+    }
+
+    private static String invalidName2(){
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().firstName()+" "+faker.name().lastName();
+
+    }
+
+    private static String invalidName3(){
+        return "ivan1 ivanov";
+
+    }
+
+    private static String invalidName4(){
+        return "ivan ivanov.";
 
     }
 
@@ -34,9 +56,24 @@ public class CardGenerator {
         return LocalDate.now().plusYears(1).format(DateTimeFormatter.ofPattern("yy"));
     }
 
-    private static String invalidYear(){
+    private static String invalidYear1(){
 
         return LocalDate.now().plusYears(6).format(DateTimeFormatter.ofPattern("yy"));
+    }
+
+    private static String invalidYear2(){
+
+        return LocalDate.now().minusYears(1).format(DateTimeFormatter.ofPattern("yy"));
+    }
+
+    private static String nowYear(){
+
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("yy"));
+    }
+
+    private static String invalidMonth(){
+
+        return LocalDate.now().minusMonths(1).format(DateTimeFormatter.ofPattern("MM"));
     }
 
     private static String validCardNumber(){
@@ -57,11 +94,57 @@ public class CardGenerator {
     }
 
     private static String cvc(){
-        Random random = new Random();
-        return String.valueOf(100 + random.nextInt(900));
+            Random random = new Random();
+            return String.valueOf(100 + random.nextInt(900));
     }
+
+    private static String invalidCvc(){
+        Random random = new Random();
+        return String.valueOf(10 + random.nextInt(90));
+    }
+
 
     public static CardData validData(){
         return new CardData(validCardNumber(), randomName(), month(), year(), cvc());
+    }
+
+    public static CardData invalidStatus(){
+        return new CardData(invalidCardNumber1(), randomName(),month(), year(),cvc());
+    }
+
+    public static CardData invalidData(){
+        return new CardData(invalidCardNumber2(), randomName(),month(), year(),cvc());
+    }
+
+    public static CardData invalidDate1(){
+        return new CardData(validCardNumber(), randomName(), invalidMonth(), nowYear(),cvc());
+    }
+
+    public static CardData invalidDate2(){
+        return new CardData(validCardNumber(), randomName(), month(), invalidYear2(),cvc());
+    }
+
+    public static CardData invalidDate3(){
+        return new CardData(validCardNumber(), randomName(), month(), invalidYear1(),cvc());
+    }
+
+    public static CardData invalidUser1(){
+        return new CardData(validCardNumber(), invalidName1(), month(), year(), cvc());
+    }
+
+    public static CardData invalidUser2(){
+        return new CardData(validCardNumber(), invalidName2(), month(), year(), cvc());
+    }
+
+    public static CardData invalidUser3(){
+        return new CardData(validCardNumber(), invalidName3(), month(), year(), cvc());
+    }
+
+    public static CardData invalidUser4(){
+        return new CardData(validCardNumber(), invalidName4(), month(), year(), cvc());
+    }
+
+    public static CardData invalidCvv(){
+        return new CardData(validCardNumber(), randomName(), month(), year(), invalidCvc());
     }
 }
--- src/test/java/ru/netology/test/AqaShopTest.java
+++ src/test/java/ru/netology/test/AqaShopTest.java
@@ -1,9 +1,11 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.page.BuyPage;
 import ru.netology.page.CreditPage;
@@ -13,11 +15,19 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AqaShopTest {
 
+
     @BeforeAll
     static void setUpAll(){
         SelenideLogger.addListener("allure", new AllureSelenide());
+        open("http://localhost:8080/");
 
 
+
+    }
+
+    @BeforeEach
+    void setUp(){
+        Selenide.refresh();
     }
 
     @AfterAll
@@ -27,12 +37,90 @@ public class AqaShopTest {
 
     @Test
 
-    public void s(){
-        open("http://localhost:8080/");
+    public void successBuyCredit(){
+
+
         var mainPage = new MainPage();
         mainPage.payByCredit();
         var credit = new CreditPage();
         credit.successBuy();
 
+    }
+
+    @Test
+    void sendWithInvalidStatus(){
+
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.blockStatus();
+    }
+
+    @Test
+    void sendWithExpired1(){
+
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.expired1();
+    }
+
+    @Test
+    void sendWithExpired2(){
+
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.expired2();
+    }
+
+    @Test
+    void sendWithTooFarYear(){
+
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.notTrueYear();
+    }
+
+    @Test
+    void sendWithJustFirstName(){
+
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.justFirstName();
+    }
+    @Test
+    void sendWithCyrillicName(){
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.nameOnCyrillic();
+    }
+
+    @Test
+    void sendWithNameWithDigit() {
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.nameWithDigit();
+    }
+
+    @Test
+    void sendWithNameWithSymbol() {
+        var mainPage = new MainPage();
+        mainPage.payByCredit();
+        var credit = new CreditPage();
+        credit.nameWithSymbol();
+    }
+
+    @Test
+    void sendWithIncorrectCvv(){
+
+            var mainPage = new MainPage();
+            mainPage.payByCredit();
+            var credit = new CreditPage();
+            credit.invalidCvv();
     }
 }
